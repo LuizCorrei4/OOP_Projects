d92a37d07de44b6fc0f59ed66762b622ec33bf0a
LuizCorrei4 <luizgcorreiadosantos@gmail.com>
2025-04-09 15:53:29 -0300

    .

--- companyProjects/empresa/src/com/empresa/funcionarios/Main.java
+++ companyProjects/empresa/src/com/empresa/funcionarios/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Funcionario estagiario = new Estagiario("Fulano", 700.0);
         Funcionario gerente = new Gerente("Ciclano", 4000.0);
-        Funcionario vendedor = new Vendedor("Vendedor", 1500.0, 4500, 0.1);
+        Funcionario vendedor = new Vendedor("Beltrano", 1500.0, 4500, 0.1);
 
-        System.out.println("Salário do estagiário: " + estagiario.calcularSalario());
-        System.out.println("Salário do gerente: " + gerente.calcularSalario());
-        System.out.println("Salário do vendedor: " + vendedor.calcularSalario());
+        System.out.println("Salário do estagiário chamado " + estagiario.getNome() + ": " + estagiario.calcularSalario() );
+        System.out.println("Salário do gerente chamado " + gerente.getNome() + ": " + gerente.calcularSalario() );
+        System.out.println("Salário do Vendedor chamado " + vendedor.getNome() + ": " + vendedor.calcularSalario() );
+        //System.out.println("Olá!")
+
     }
 }
